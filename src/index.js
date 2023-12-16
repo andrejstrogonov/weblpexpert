74d8ef126a4298f8493eb01a9ff853631b19bf21
andrejstrogonov <strogonovandrej@hotmail.com>
2023-12-16 16:00:32 +0300

    First prototype

--- src/index.js
+++ src/index.js
@@ -6,21 +6,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './NavigationBar';
 import ReverseContainer from './ReverseContainer'
 import Variants from './Variants';
-import store from './app/store'
-import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store = {store}>
     <React.StrictMode>
       <NavigationBar/>
       <ReverseContainer/>
       <Variants/>
     </React.StrictMode>
-  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
