f81c737776b8f0bccfe954adccb99fba44ed0d81
andrejstrogonov <strogonovandrej@hotmail.com>
2024-01-17 15:46:17 +0300

    Corrections in navigation bar

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,23 +1,40 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
 
 function NavigationBar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-    <Container>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Главная</Nav.Link>
-          <Nav.Link href="#сlassic">Классический вывод</Nav.Link>
-          <Nav.Link href="#relevance">Релевантный вывод</Nav.Link>
-          <Nav.Link href="#clusterrelevance">Кластерно-релевантный</Nav.Link>
-          <Nav.Link href="#neuro">Нейросетевой</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+    <>
+      <Navbar className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home">Главная</Navbar.Brand>
+          </Container>
+      </Navbar>
+      <br />
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#сlassic">Классический вывод</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <br />
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#relevance">Релевантный вывод</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <br />
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#clusterrelevance">Кластерно-релевантный</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <br />
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#neuro">Нейросетевой</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
